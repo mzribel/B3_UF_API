dd9a515cd59ae9a86be5fa2f22cbc2f71ec4aaeb
Tony-De-Donato <tonydedo13@gmail.com>
2025-05-08 16:05:39 +0200

    initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/test/java/projet/uf/B3UfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.uf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3UfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
