6f3351af22b8125cf390435779d15b6f0ed1d39a
RathGate <mariannecorbel@hotmail.fr>
2025-05-25 12:30:37 +0200

    fix(auth): added not blank constraints on registercommand

--- src/main/java/projet/uf/modules/auth/application/ports/in/RegisterCommand.java
+++ src/main/java/projet/uf/modules/auth/application/ports/in/RegisterCommand.java
@@ -6,8 +6,12 @@ import jakarta.validation.constraints.Size;
 
 public record RegisterCommand(
         @Email(message = "Format de l'email invalide")
-        @NotBlank String email,
-        @NotBlank String password,
+        @NotBlank(message = "L'email ne peut pas être vide")
+        String email,
+
+        @NotBlank(message = "Le mot de passe ne peut pas être vide")
+        String password,
+
         @Size(max = 20, message = "Le nom d'affichage ne peut pas dépasser 20 caractères")
         String displayName
 ) {}
